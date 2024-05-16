8a88764fa0d3ea16e3fe3e48213f8e396c83f576
Farhan <124482863+FarAwayFromLife@users.noreply.github.com>
2024-05-17 00:51:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function scrollToPricingSection() {
+    var pricingSection = document.getElementById("pricingSection");
+    pricingSection.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToAboutSection(){
+    
+    var aboutSection = document.getElementById("aboutSection");
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+
+}
+function scrollToEmailSection(){
+    
+    var emailSection = document.getElementById("emailSection");
+    emailSection.scrollIntoView({ behavior: 'smooth' });
+
+}
+
